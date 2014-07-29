5dc7f29862fd835c5975fd4c950d71c016336411
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2014-07-29 16:56:45 -0400

    v0.1.0 deployed

---
+++ src/main/java/org/chrisjr/topic_annotator/Main.java
@@ -0,0 +1,18 @@
+package org.chrisjr.topic_annotator;
+
+/**
+ * @author chrisjr
+ * 
+ *         Minimal Java class, used so that we generate javadoc and sources jars
+ *         for Sonatype deployment.
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		// does nothing
+
+	}
+
+}
